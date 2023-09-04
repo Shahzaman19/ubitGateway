2b37b5689ef494b91ecdce0dd2199a49b32f810a
Shahzaman <shahzaman.aftab@kodexolabs.com>
2023-09-05 00:32:28 +0500

    fix : Dlt unused stuff

--- Apps/routes/app.routes.js
+++ Apps/routes/app.routes.js
@@ -4,8 +4,6 @@ let router = express.Router();
 router.use('/users',require('./user.routes'))
 router.use('/job', require('./job-posting.routes'))
 router.use('/job', require('./job-Application.routes'))
-router.use('/auth', require('./googleAuth.routes'))
-router.use('/auth', require('./linkedinAuth.routes'))
 
 
 module.exports = router;
